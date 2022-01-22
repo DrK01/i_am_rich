0d76cc4cd059b8e8f6e19d497e63e31006e66aae
Omar Monrroy <45756472+Drk01@users.noreply.github.com>
2022-01-22 01:01:14 -0600

    Hello World from Flutter, I Am Rich

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+    backgroundColor: Colors.black,
+    appBar: AppBar(
+      title: Text('I Am Rich'),
+    ),
+    body: Center(
+      child: Image.network(
+          "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fmedia.giphy.com%2Fmedia%2FMO59PKZF5SXm%2Fgiphy.gif&f=1&nofb=1"),
+    ),
+  )));
+}
