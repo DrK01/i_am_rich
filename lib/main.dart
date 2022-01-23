4982ae7918da2a8d49fd30d4cf404781c9023733
Omar Monrroy <45756472+Drk01@users.noreply.github.com>
2022-01-23 00:18:37 -0600

    Fixing identation

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(
+    MaterialApp(
       home: Scaffold(
-    backgroundColor: Colors.black,
-    appBar: AppBar(
-      title: Text('I Am Rich'),
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+        ),
+        body: Center(
+          child: Image.network(
+              "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fmedia.giphy.com%2Fmedia%2FMO59PKZF5SXm%2Fgiphy.gif&f=1&nofb=1"),
+        ),
+      ),
     ),
-    body: Center(
-      child: Image.network(
-          "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fmedia.giphy.com%2Fmedia%2FMO59PKZF5SXm%2Fgiphy.gif&f=1&nofb=1"),
-    ),
-  )));
+  );
 }
